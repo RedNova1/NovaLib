e2e814023125f6374ce991751bd890a425588dfa
RedNova-therealonenotthefakeoneokay <115672536+RedNova-therealonenotthefakeoneokay@users.noreply.github.com>
2023-07-31 20:40:52 -0500

    Plugin Blacklist System
    broken

--- NovaLib.cs
+++ NovaLib.cs
@@ -2,17 +2,22 @@
 using BepInEx.Bootstrap;
 using BepInEx.Logging;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace NovaLib
 {
  public class Anticheat : MonoBehaviour
     {
+        
+
         [SerializeField] PlayerUtilities playerUtil;
         [SerializeField] OnCheatsEnable cheats;
-        [SerializeField] OptionsManager pauseMenu;
-        
+        OptionsManager pauseMenu;
+
         [Tooltip("Plug Canvas Blocker Into here")] [SerializeField] GameObject missingDepUI; // if you wish to stop the game if you don't have the mod, put your Canvas Blocker in here.
+
+        [SerializeField] List<string> blacklist;
         void prereq() // Disable the blocker UI if present
         {
             if (missingDepUI != null) { missingDepUI.gameObject.SetActive(false); playerUtil.EnablePlayer(); }
@@ -21,17 +26,20 @@ namespace NovaLib
         
         
         [Header("Module Toggles")]
-        [Tooltip("May cause unforseen consequences")]public bool TimeScaleLocked; //toggle for locking time scale
-        [SerializeField] bool blockUnityExplorer;
-        public void unityExplorerCheck() {
-            foreach (var plugin in Chainloader.PluginInfos)
+        [Tooltip("May cause unforseen consequences")] public bool TimeScaleLocked; //toggle for locking time scale
+        [Tooltip("Input plugin GUIDS here")][SerializeField] bool blacklistPlugins;
+        public void blacklistCheck() {
+            if (blacklistPlugins == true)
             {
-                var metadata = plugin.Value.Metadata;
-                if (metadata.GUID.Equals("com.sinai.unityexplorer"))
+                foreach (var plugin in Chainloader.PluginInfos)
                 {
-                    Console.WriteLine($"Found {"com.sinai.unityexplorer"}, quitting");
-                    AnticheatActivate();
-                    break;
+                    var metadata = plugin.Value.Metadata;
+                    if (blacklist.Contains(metadata.GUID))
+                    {
+                        Console.WriteLine($"Found {metadata.GUID}");
+                        //AnticheatActivate();
+                        break;
+                    }
                 }
             }
         }
@@ -44,7 +52,7 @@ namespace NovaLib
 
         [Header("TimeScale Lock")]
         [Min(0.1f)] public float timeScaleSet = 1; //what the timescale is locked to
-        void timeScaleLock () { if (TimeScaleLocked == true && pauseMenu.paused == false) { Time.timeScale = timeScaleSet; } }
+        void timeScaleLock () { if (TimeScaleLocked == true && pauseMenu.paused == false) { Time.timeScale = timeScaleSet * Time.deltaTime; } }
         public void changeTimeScale(float timeScale)
         {
             timeScaleSet = timeScale;
@@ -56,7 +64,7 @@ namespace NovaLib
         void Start()
         {
             prereq();
-            unityExplorerCheck();
+            blacklistCheck();
             cheatBlocker();
         }
         
